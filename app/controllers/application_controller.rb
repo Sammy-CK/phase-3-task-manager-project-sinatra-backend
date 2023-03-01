dbd398f4f68584f939cea6e5b549f7a81c6aa4ca
Sammy-CK <sammycherono2696@gmail.com>
2023-03-01 06:17:28 +0300

    Added User endpoints for create task, update task, get tasks and delete task

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, "application/json"
 
-  # User sign in
+  # user sign in
   patch "/login" do
     user = User.find_by(name: params[:name], password: params[:password])
-    { isRegistered: "#{!!user}" }.to_json
+    { isRegistered: "#{!!user}", userId: user.id }.to_json
   end
 
-  #User sign up
+  # user sign up
   post "/signup" do
     is_signed_up = User.find_by(email: params[:email])
     if is_signed_up
@@ -18,7 +18,41 @@ class ApplicationController < Sinatra::Base
     end
   end
 
+  # user create task
+  post "/tasks/create" do
+    task = Task.create(
+      name: params[:name],
+      description: params[:description],
+      due: params[:due],
+      status: params[:status],
+      user_id: params[:user_id],
+    )
+    task.to_json
+  end
 
+  # user updates task
+  patch "/tasks/update/:id" do
+    task = Task.find(params[:id])
+    task.update(status: params[:status])
+    task.to_json
+  end
 
+  # user gets individual task
+  get "/tasks/:id" do
+    task = Task.find(params[:id])
+    task.to_json
+  end
 
+  # user lists all their own tasks
+  patch "/tasks/" do
+    task = Task.where(user_id: params[:user_id])
+    task.to_json
+  end
+
+  # user delete specific tasks
+  delete "/tasks/:id" do
+    task = Task.find(params[:id])
+    task.destroy
+    task.to_json
+  end
 end
